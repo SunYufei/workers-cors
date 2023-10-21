7e8a479f87ae24eedb79ad04f51403ac376fe599
SunYufei <sunyufei963@qq.com>
2023-10-21 12:18:19 +0800

    socket connect test

--- tunnel/src/index.ts
+++ tunnel/src/index.ts
@@ -1,4 +1,5 @@
-import { errorResponse, jsonResponse } from './resp'
+import { connect } from 'cloudflare:sockets'
+import { errorResponse, jsonResponse, textResponse } from './resp'
 import { notValidUUID } from './util'
 
 export interface Env {
@@ -26,6 +27,7 @@ export default {
       }
       if (pathname == '/connect') {
          // Socket connect test
+         return socketTest('cloudflare.com')
       }
       if (pathname == `/${userId}`) {
          // VLESS config HTML
@@ -34,4 +36,23 @@ export default {
       // Note: remove any other hostname and caching process
       return fetch('https://global.cctv.com/')
    },
+}
+
+async function socketTest(hostname: string, port = 80): Promise<Response> {
+   console.log(`Connecting to ${hostname} ${port}`)
+   const socket = connect({ hostname: hostname, port: port })
+   const writer = socket.writable.getWriter()
+   const reader = socket.readable.getReader()
+   try {
+      await writer.write(new TextEncoder().encode(`GET / HTTP/1.1\r\nHost: ${hostname}\r\n\r\n`))
+      writer.releaseLock()
+      const { value } = await reader.read()
+      return textResponse(new TextDecoder().decode(value))
+   } catch (e) {
+      return errorResponse(JSON.stringify(e))
+   } finally {
+      writer.releaseLock()
+      reader.releaseLock()
+      await socket.close()
+   }
 }
